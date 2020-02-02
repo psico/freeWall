e46487d26ad050a22044c926db264093e7ff0c09
João Gabriel <desenvolvedorjg@gmail.com>
2020-02-02 16:07:25 -0300

    [DEL] Component react-router-dom don't work on mobile app, only web.

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component}  from 'react';
+import Test from "./src/components/Test";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Test}/>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
