a0bdf2978d26a56bd3fab8f4c6e25d5348ddac73
João Gabriel <desenvolvedorjg@gmail.com>
2020-02-05 22:35:30 -0300

    [ADD] Added a radio button component and the begining implemetion.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
+import RadioForm, {RadioButton} from "react-native-simple-radio-button";
+
+var radio_props = [
+    {label: 'My Pictures', value: 0},
+    {label: 'Web Pictures', value: 1}
+];
 
 export class Home extends Component {
 
     render() {
-        return(
-        <View>
-            <Text>Home</Text>
-        </View>
-    );
+        return (
+            <View>
+                <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    onPress={(value) => {
+                        this.setState({value: value})
+                    }}
+                />
+            </View>
+        );
     }
-
 }
