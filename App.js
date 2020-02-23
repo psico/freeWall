6fed86432f4bb42cfc11c62e2342c09276ea83a2
João Gabriel <desenvolvedorjg@gmail.com>
2020-02-22 23:53:13 -0300

    [ADD] Begin to implemts new naviagtion using function.

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import React, {Component} from 'react';
-import { createAppContainer } from 'react-navigation';
-import AppNavigator from "./src/components/Navigation/Navigation";
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from "./src/components/Home/Home";
 
-const AppContainer = createAppContainer(AppNavigator);
-
-export default class App extends Component {
-    render() {
-        return <AppContainer/>
-    }
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
