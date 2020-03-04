e357232515686ee8cd39c2e8cf3243160c5682b1
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-03 21:29:40 -0300

    [UPT] Another try and didn't work, I will turn back for another modules.

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from "./src/components/Home/Home";
+import React, {Component} from 'react';
+// import { View, Text } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import Home from "./src/components/Home/Home";
 
-const Stack = createStackNavigator();
+import { View, TouchableOpacity, Text } from 'react-native';
+import ManageWallpaper, { TYPE } from 'react-native-manage-wallpaper';
 
-function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+export default class App extends Component<> {
+    _callback = res => {
+        console.log('Response: ', res);
+    };
+
+    _setWallpaper = () => {
+        ManageWallpaper.setWallpaper(
+            {
+                uri: 'https://i.pinimg.com/originals/76/5e/1d/765e1dc8cb1cc115fb3b0b39a895fdeb.jpg',
+            },
+            this._callback,
+            TYPE.HOME,
+        );
+    };
+
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#F5FCFF',
+                }}
+            >
+                <TouchableOpacity
+                    style={{
+                        paddingHorizontal: 30,
+                        paddingVertical: 8,
+                        marginBottom: 24,
+                        borderRadius: 16,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: '#000000',
+                    }}
+                    onPress={this._setWallpaper}
+                >
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            margin: 10,
+                            textAlign: 'center',
+                            color: '#FFFFFF',
+                        }}
+                    >
+                        Change Home Wallpaper
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
-
-export default App;
